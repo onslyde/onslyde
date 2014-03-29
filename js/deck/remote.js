bb8bf80bbe59de532692c441eaaccd26bde2d8f5
wesleyhales <wesleyhales@gmail.com>
2014-03-28 17:08:40 -0700

    turn off oauth on normal remote

--- js/deck/remote.js
+++ js/deck/remote.js
@@ -36,27 +36,17 @@ function sendVote(event,option){
 }
 
 option1.onclick = function(event) {
-  if (window.userObject.name === '') {
-    option2.onclick = onslyde.oauth.handleAuthClick;
-  } else {
     _gaq.push(['_trackEvent', 'onslyde-option1', 'vote']);
     sendVote(event,option1.value);
     disablePoll();
     return false;
-  }
-
 };
 
 option2.onclick = function(event) {
-  if (window.userObject.name === '') {
-    option2.onclick = onslyde.oauth.handleAuthClick;
-  } else {
     _gaq.push(['_trackEvent', 'onslyde-option2', 'vote']);
     sendVote(event,option2.value);
     disablePoll();
     return false;
-  }
-
 };
 
 
